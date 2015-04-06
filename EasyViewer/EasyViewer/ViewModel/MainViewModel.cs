f1264519ca982a1b1567e510b2dad6a2356822a5
Ishwarya Lakshmi <LakshmiI@advisory.com>
2015-04-06 15:51:03 +0530

    populating system databases in the dropdown list box

--- EasyViewer/EasyViewer/ViewModel/MainViewModel.cs
+++ EasyViewer/EasyViewer/ViewModel/MainViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Windows.Documents;
 using EasyViewer.Dto;
 using GalaSoft.MvvmLight;
 using EasyViewer.Model;
@@ -15,7 +19,8 @@ namespace EasyViewer.ViewModel
         public RelayCommand ExecuteQuery { get; private set; }
         public RelayCommand<DataGridAutoGenerateCommandArgs> AutoGenerateColumn { get; private set; }
         public RelayCommand<DataGridDoubleClickCommandArgs> DoubleClickCommand { get; private set; } 
-        public ObservableCollection<QueryData> DataItems { get; set; } 
+        public ObservableCollection<QueryData> DataItems { get; set; }
+        public List<String> DataBases { get; set; }
 
         public MainViewModel(IDataService dataService)
         {
@@ -24,6 +29,9 @@ namespace EasyViewer.ViewModel
             AutoGenerateColumn = new RelayCommand<DataGridAutoGenerateCommandArgs>(AutoGenerateColumnHandler);
             DoubleClickCommand = new RelayCommand<DataGridDoubleClickCommandArgs>(DataGridDoubleClickHandler);
             DataItems = new ObservableCollection<QueryData>();
+            DataBases = new List<string>();
+
+           FetchDatabasesQuery();
         }
 
         ////public override void Cleanup()
@@ -33,15 +41,28 @@ namespace EasyViewer.ViewModel
         ////    base.Cleanup();
         ////}
 
+        public void FetchDatabasesQuery()
+        {
+            var data = _dataService.FetchQueryData("master", "select name from sys.databases");
+            var dataTable = data.QueryDataTable;
+            var datatableRow = dataTable.Rows;
+            foreach (DataRow dr in datatableRow)
+            {
+
+                var a = dr.ItemArray[0];
+                DataBases.Add(a.ToString());
+            }
+        }
+
         public void ExecuteDbQuery()
         {
-            var data = _dataService.FetchQueryData();
+            var data = _dataService.FetchQueryData("", "");
             DataItems.Add(data);
         }
 
         public void AutoGenerateColumnHandler(DataGridAutoGenerateCommandArgs e)
         {
-            var foreignKeyMetadata = _dataService.GetForeignKeyMetaData();
+            var foreignKeyMetadata = _dataService.GetForeignKeyMetaData("");
             ForeignKeyMetaData data = foreignKeyMetadata.FirstOrDefault(x => x.CurrentColumn == e.ColumnEventArgs.PropertyName);
 
             if (data != null)
